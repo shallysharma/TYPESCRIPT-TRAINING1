8c02ac8667b29a28ce712d03029591b200038225
Shally Sharma <shallysharma2409@gmail.com>
2021-11-01 19:03:53 -0400

    first commit

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstname = "Shally";
+var lastname = ' Sharma';
+console.log(found);
+console.log("The grade is " + grade);
+console.log(" Hi " + firstname + " " + lastname);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean =true;
+let grade: number = 88.6;
+let firstname: string = "Shally";
+let lastname: string = ' Sharma';
+
+console.log(found);
+console.log("The grade is " + grade );
+console.log(" Hi " + firstname + " " + lastname ); 
